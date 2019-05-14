6f266b9d7b01e41bad984b1cdbb9c2282509fbd4
washingtk <nhppr1997@icloud.com>
2019-05-14 12:22:23 +0200

    em-algorithm

--- MCMC/EM_method/em_algorithm.py
+++ MCMC/EM_method/em_algorithm.py
@@ -2,17 +2,21 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
+# load data and see a distribution with histogram
 obs = np.loadtxt('mixture-observations.csv', delimiter=',')
 plt.figure(1)
 plt.hist(obs, label="distribution of obs")
 plt.legend()
 plt.show()
 
+# updating theta using EM method
 iteration = 50
 theta = np.zeros(iteration)
 theta[0] = 0.5
 for i in range(1, iteration):
     theta[i] = np.sum((theta[i-1]*norm.pdf(obs, 1, 2) / (theta[i-1]*norm.pdf(obs, 1, 2) + (1-theta[i-1])*norm.pdf(obs, 0, 1)))) / len(obs)
+
+# see result
 plt.figure(2)
 plt.plot(theta, label="theta updating")
 plt.grid()
